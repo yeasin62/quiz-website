6dfe9dd796df0d5a71d31509296fb894f26264f7
yeasin62 <gazi.yeasin@yahoo.com>
2022-10-16 01:50:35 +0600

    added rechard in statistics

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import axios from 'axios';
+import React, { PureComponent, useEffect, useState } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const [data,setData] = useState([]);
+
+    useEffect(()=>{
+        axios.get('https://openapi.programming-hero.com/api/quiz')
+        .then(data => {
+            const quizLoaded = data.data.data;
+            const quizData = quizLoaded.map(quiz => {
+                const quizName = quiz.name;
+                const totalQuiz = quiz.total;
+                const totalData = {
+                    name: quizName,
+                    total: totalQuiz
+                }
+                return totalData;
+            })
+            setData(quizData);
+        })
+    },[])
+    
+
     return (
         <div>
-            hello statictics
+            <h2>Statistics</h2>
+
+            <BarChart width={400} height={400} data={data}>
+            <Bar dataKey="total" fill="#8884d8" />
+            <XAxis dataKey="name"></XAxis>
+            <YAxis dataKey="total"></YAxis>
+            <Tooltip></Tooltip>
+            </BarChart>
         </div>
     );
 };
