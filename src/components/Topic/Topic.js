aa51c816535b7fd5750e8e44ef38045477a3b33a
yeasin62 <gazi.yeasin@yahoo.com>
2022-10-15 15:12:55 +0600

    home route api loaded

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Col,Card, Button } from 'react-bootstrap';
+import { Card, Col,Button } from 'react-bootstrap';
 import './Topic.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Topic = ({topic}) => {
-    const {name, id, logo, total} = topic;
+    const {name, logo, total} = topic;
     return (
         <Col>
             <Card>
                 <Card.Img variant="top" src={logo} />
                 <Card.Body>
                 <Card.Title>{name}</Card.Title>
-                <Card.Text>
-                    {total}
-                </Card.Text>
-                <Button>Start Practice</Button>
+                <Button> Start Practice <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></Button>
                 </Card.Body>
+                <Card.Text>
+                    Total Questions: {total}
+                </Card.Text>
             </Card>
         </Col>
     );
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 import Topic from '../Topic/Topic';
+import './Topics.css';
 
 
 
@@ -12,7 +13,10 @@ const Topics = () => {
     return (
         <div>
             <Container>
-                <Row xs={1} md={2} xl={4} className="g-4">
+                <div className="title">
+                    <h1>Welcome To Test Competition</h1>
+                </div>
+                <Row xs={1} md={2} xl={4} className="g-4 gcards">
                     {
                         subjects.map(topic => <Topic key={topic.id} topic={topic}></Topic>)
                     }
